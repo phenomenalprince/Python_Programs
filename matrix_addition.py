a1051a64780c4c6c1d8d898ebda05bd3cff57966
phenomenalprince <54848663+phenomenalprince@users.noreply.github.com>
2019-12-22 18:14:47 +0530

    Create matrix_addition.py

---
+++ matrix_addition.py
@@ -0,0 +1,12 @@
+x = [[1,2,3],[4,5,6],[7,8,9]]
+y = [[1,2,3],[4,5,6],[7,8,9]]
+result = [[0,0,0],[0,0,0],[0,0,0]]
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] + y[i][j]
+for r in result:
+    print(r)
+
+import time
+start_time = time.time()
+print("--- %s seconds ---" % (time.time() - start_time))
